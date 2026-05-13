d7b23d1e3b800ffaf05e307f3452c2c7f41a616f
jem-Asus <jeremychuahtm@gmail.com>
2026-05-13 15:44:44 +0800

    feat: working profiler that scans the db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from src.ingestor import ingest_all_mhtml
 from src.processor import process_all_html
 from src.loader import load_all_jsons
+from src.profiler import run_data_profile
 
 def ingest():
     ingest_all_mhtml("data/0_source", "data/1_bronze")
@@ -12,17 +13,31 @@ def process():
 def load():
     load_all_jsons("data/2_silver", "data/3_gold")
 
+def profile():
+    run_data_profile("data/3_gold/jobs.db")
+
+def all():
+    ingest()
+    print("\n")
+    process()
+    print("\n")
+    load()
+    print("\n")
+    profile()
+
 commands = {
     "ingest": ingest,
     "process": process,
-    "load": load
+    "load": load,
+    "profile": profile,
+    "all": all
 }
 
 def main():
     if len(sys.argv) == 2:
         commands.get(sys.argv[1], lambda: print("Unknown command"))()
     else:
-        print("Usage:", sys.argv[0], "<command>")
+        print("Usage: python", sys.argv[0], "[ingest|process|load|profile|all]")
 
 
 if __name__ == "__main__":
---
+++ src/profiler.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import sqlite3
+
+def run_data_profile(db_path):
+    db_path = Path(db_path)
+
+    if not db_path.is_file():
+        print("❌ Database not found at", db_path)
+        return
+
+    try:
+        database = sqlite3.connect(db_path)
+        cursor = database.cursor()
+
+        total          = fetch_from_database_query(cursor, "SELECT COUNT(*) from jobs")
+        null_job_title = fetch_from_database_query(cursor, "SELECT COUNT(*) from jobs WHERE job_title IS NULL")
+        null_company   = fetch_from_database_query(cursor, "SELECT COUNT(*) from jobs WHERE company IS NULL")
+        null_desc      = fetch_from_database_query(cursor, "SELECT COUNT(*) from jobs WHERE description IS NULL")
+        avg_desc       = fetch_from_database_query(cursor, "SELECT CAST(ROUND(AVG(LENGTH(description))) AS INTEGER) from jobs")
+
+        shortest       = fetch_from_database_query(cursor, """
+                                                        SELECT source_id, job_title, LENGTH(description)
+                                                        FROM jobs
+                                                        WHERE LENGTH(description) = (
+                                                        SELECT MIN(LENGTH(description)) FROM jobs
+                                                        )
+                                                        """)
+        longest        = fetch_from_database_query(cursor, """
+                                                        SELECT source_id, job_title, LENGTH(description)
+                                                        FROM jobs
+                                                        WHERE LENGTH(description) = (
+                                                        SELECT MAX(LENGTH(description)) FROM jobs
+                                                        )
+                                                        """)
+        shortest_desc_id    = shortest[0]
+        shortest_desc_title = shortest[1]
+        shortest_desc       = shortest[2]
+        longest_desc_id     = longest[0]
+        longest_desc_title  = longest[1]
+        longest_desc        = longest[2]
+
+        database.close()
+
+        print(
+            f"--- 🔍 DATA QUALITY REPORT ---",
+            f"📈 Total Records: {total}",
+            f"❓ Missing Values -> job_title: {null_job_title}, company: {null_company}, description: {null_desc}",
+            f"📝 Avg Description Length: {avg_desc} chars",
+            f"⚠️  Shortest Description: {shortest_desc} chars",
+            f"  ↳ source_id: {shortest_desc_id} | job_title: {shortest_desc_title}",
+            f"🚨 Longest Description: {longest_desc} chars",
+            f"  ↳ source_id: {longest_desc_id} | job_title: {longest_desc_title}",
+            sep="\n")
+    except Exception as e:
+        print(e)
+
+def fetch_from_database_query(cursor, query):
+    cursor.execute(query)
+    output = cursor.fetchone()
+    if len(output) == 1:
+        return output[0]
+    return output
